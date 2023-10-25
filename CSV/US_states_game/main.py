26e4d6058dede626a0509a3c3babbe1eaf242cb1
Mohammad Bakir <85129393+Mohammad-bakeer@users.noreply.github.com>
2023-10-25 18:55:21 +0300

    added list comprehension to the code line 19

--- CSV/US_states_game/main.py
+++ CSV/US_states_game/main.py
@@ -16,11 +16,7 @@ while len(guessed_states)<50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States correct", prompt="What's another state's name?").title()
     
     if answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
-            
+        missing_states = [state for state in all_states if state not in guessed_states]
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("CSV/US_states_game/states_missed.csv")
         break
