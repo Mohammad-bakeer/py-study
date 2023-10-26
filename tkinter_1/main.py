88a40bfb802baa69edee1427b2f064c66a33ad7f
Mohammad Bakir <85129393+Mohammad-bakeer@users.noreply.github.com>
2023-10-26 15:19:16 +0300

    some changes for the style of the code

--- tkinter_1/main.py
+++ tkinter_1/main.py
@@ -8,22 +8,24 @@ input = Entry(width=18)
 input.grid(column=1, row=0)
 
 miles_label = Label(text="Miles", font=("Arial", 15, "normal"))
-miles_label.grid(column=2,row=0)
+miles_label.grid(column=2, row=0)
 
-label_1 = Label(text="is equal to: ",font=("Arial", 15, "normal"))
-label_1.grid(column=0,row=1)
+label_1 = Label(text="is equal to: ", font=("Arial", 15, "normal"))
+label_1.grid(column=0, row=1)
 
 label_2 = Label(text=0, font=("Arial", 15, "normal"))
-label_2.grid(column=1,row=1)
+label_2.grid(column=1, row=1)
 
 label_3 = Label(text="Km", font=("Arial", 15, "normal"))
-label_3.grid(column=2,row=1)
+label_3.grid(column=2, row=1)
+
 
 def convert():
-    x= (float(input.get())*1.609)
+    x = (float(input.get())*1.609)
     label_2.config(text=x)
 
+
 button = Button(text="Calculate", command=convert)
-button.grid(column=1,row=2)
+button.grid(column=1, row=2)
 
 window.mainloop()
