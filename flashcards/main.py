363be8dac3266c76047a5599da90908bb08a482b
Mohammad Bakir <85129393+Mohammad-bakeer@users.noreply.github.com>
2023-11-03 19:34:36 +0300

    improved the code a bit

--- flashcards/main.py
+++ flashcards/main.py
@@ -3,9 +3,15 @@ import pandas
 import random
 BACKGROUND_COLOR = "#B1DDC6"
 current_card = {}
+to_learn={}
 
-data = pandas.read_csv("flashcards/data/french_words.csv")
-to_learn = data.to_dict(orient="records")
+try:
+    data = pandas.read_csv("flashcards/data/words_to_learn.csv")
+except:
+    original_data = pandas.read_csv("flashcards/data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
 
 def next_card():
     global current_card,flip_timer
@@ -21,6 +27,13 @@ def flip_card():
     canvas.itemconfig(card_titel,text="English",fill="white")
     canvas.itemconfig(card_word,text=current_card["English"],fill="white")
     canvas.itemconfig(card_image, image=back_image)
+
+def is_known():
+    to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("flashcards/data/words_to_learn.csv")
+    next_card()
+
 
 window = Tk()
 window.title("Flashy")
@@ -44,7 +57,7 @@ unknown_button.grid(row=1,column=0)
 
 
 check_image = PhotoImage(file="flashcards/images/right.png")
-known_button = Button(image=check_image,highlightthickness=0)
+known_button = Button(image=check_image,highlightthickness=0,command=is_known)
 known_button.grid(row=1, column= 1)
 
 next_card()
