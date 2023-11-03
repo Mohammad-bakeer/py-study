031b35ac6f1b4f7807b6475be043bbcbd6bbd9af
Mohammad Bakir <85129393+Mohammad-bakeer@users.noreply.github.com>
2023-11-03 19:37:23 +0300

    fixed error, would add index to the new csv each time the code runs

--- flashcards/main.py
+++ flashcards/main.py
@@ -31,7 +31,7 @@ def flip_card():
 def is_known():
     to_learn.remove(current_card)
     data = pandas.DataFrame(to_learn)
-    data.to_csv("flashcards/data/words_to_learn.csv")
+    data.to_csv("flashcards/data/words_to_learn.csv", index=False)
     next_card()
 
 
