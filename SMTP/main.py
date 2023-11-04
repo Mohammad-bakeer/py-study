d171ecd4de763bcd2db910e0060ef0fba5b0696b
Mohammad Bakir <85129393+Mohammad-bakeer@users.noreply.github.com>
2023-11-04 11:50:48 +0300

    basic structure for the birthday code

--- SMTP/main.py
+++ SMTP/main.py
@@ -1,10 +1,22 @@
 import smtplib
+import datetime as dt
+import random
 
 my_email= "test.1xm2@gmail.com"
 password = "ylqglrmqjxspgenv"
 
-connection = smtplib.SMTP("smtp.gmail.com", 587)
-connection.starttls()
-connection.login(user=my_email,password=password)
-connection.sendmail(from_addr=my_email,to_addrs="bakeermohammad11@gmail.com",msg="Helloo")
-connection.close()
+now = dt.datetime.now()
+weekday = now.weekday()
+print(weekday)
+if(weekday==5): 
+    with open(file="SMTP/quotes.txt") as qf:
+        all_quotes = qf.readlines()
+        quote = random.choice(all_quotes)
+        print(quote)
+
+
+with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+    connection.starttls()
+    connection.login(user=my_email,password=password)
+    connection.sendmail(from_addr=my_email,to_addrs="bakeermohammad11@gmail.com",msg="Subject:Helloo\n\nthis is the text")
+    
