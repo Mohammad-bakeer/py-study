01cfc39a011bdbbe002590b1fc257961741ab2e6
Mohammad Bakir <85129393+Mohammad-bakeer@users.noreply.github.com>
2023-11-04 11:57:45 +0300

    depending on the day will send random quote

--- SMTP/main.py
+++ SMTP/main.py
@@ -6,17 +6,16 @@ my_email= "test.1xm2@gmail.com"
 password = "ylqglrmqjxspgenv"
 
 now = dt.datetime.now()
+print(now.day)
 weekday = now.weekday()
-print(weekday)
+
 if(weekday==5): 
     with open(file="SMTP/quotes.txt") as qf:
         all_quotes = qf.readlines()
         quote = random.choice(all_quotes)
-        print(quote)
 
-
-with smtplib.SMTP("smtp.gmail.com", 587) as connection:
-    connection.starttls()
-    connection.login(user=my_email,password=password)
-    connection.sendmail(from_addr=my_email,to_addrs="bakeermohammad11@gmail.com",msg="Subject:Helloo\n\nthis is the text")
-    
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(user=my_email,password=password)
+        connection.sendmail(from_addr=my_email,to_addrs="bakeermohammad11@gmail.com",msg=f"Subject:{now.day}-{now.month}-{now.year} Quote\n\n{quote}")
+        
