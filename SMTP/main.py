fe970194b56f20196199d4e96ed62e3a5d559f3e
Mohammad Bakir <85129393+Mohammad-bakeer@users.noreply.github.com>
2023-11-04 13:39:38 +0300

    now we can send birthday mails when its said persons birthday

--- SMTP/main.py
+++ SMTP/main.py
@@ -1,21 +1,28 @@
 import smtplib
-import datetime as dt
+from datetime import datetime 
 import random
+import pandas
 
 my_email= "test.1xm2@gmail.com"
 password = "ylqglrmqjxspgenv"
 
-now = dt.datetime.now()
-print(now.day)
-weekday = now.weekday()
+now = datetime.now()
+today = (now.month,now.day)
 
-if(weekday==5): 
-    with open(file="SMTP/quotes.txt") as qf:
-        all_quotes = qf.readlines()
-        quote = random.choice(all_quotes)
+data = pandas.read_csv("SMTP/birthdays.csv")
+birthday_dict = {(data_row["month"],data_row["day"]):data_row for (index, data_row) in data.iterrows()}
+
+if today in birthday_dict:
+    birthday_person = birthday_dict[today]
+    print(birthday_person["email"])
+    file_path = f"SMTP/letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file=file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]",birthday_person["name"])
+
 
     with smtplib.SMTP("smtp.gmail.com", 587) as connection:
         connection.starttls()
         connection.login(user=my_email,password=password)
-        connection.sendmail(from_addr=my_email,to_addrs="bakeermohammad11@gmail.com",msg=f"Subject:{now.day}-{now.month}-{now.year} Quote\n\n{quote}")
+        connection.sendmail(from_addr=my_email,to_addrs=birthday_person["email"],msg=f"Subject:Happy Birthday\n\n{contents}")
         
