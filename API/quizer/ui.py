0edb91a72447568acd67e8db3f748034edeb836e
Mohammad Bakir <85129393+Mohammad-bakeer@users.noreply.github.com>
2023-11-06 14:33:09 +0300

    giving the pady to canvas is better

--- API/quizer/ui.py
+++ API/quizer/ui.py
@@ -9,12 +9,12 @@ class QuizInterface:
         self.window.title("Quizzler")
         self.window.config(bg=THEME_COLOR, padx=20, pady=20)
         
-        self.score_label = Label(text="score : 0",bg=THEME_COLOR,pady=20, font=("Arial",12,"normal"))
+        self.score_label = Label(text="score : 0",bg=THEME_COLOR, font=("Arial",12,"normal"))
         self.score_label.grid(row=0, column=1)
 
         self.canvas = Canvas(height=250, width=300,)
         self.qc_text = self.canvas.create_text(150,125,text="j", font=FONT)
-        self.canvas.grid(row=1,column=0,columnspan=2)
+        self.canvas.grid(row=1,column=0,columnspan=2, pady=50)
         
         self.check_image = PhotoImage(file="API/quizer/images/true.png")
         self.true_button = Button(image=self.check_image)
