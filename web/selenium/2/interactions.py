a6b1a64beeab3c8f8e6eb1b7c074faeee6cbd9c2
Mohammad Bakir <85129393+Mohammad-bakeer@users.noreply.github.com>
2023-11-19 23:00:49 +0300

    more of it

--- web/selenium/2/interactions.py
+++ web/selenium/2/interactions.py
@@ -1,8 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By  
+from selenium.webdriver.common.keys import Keys
 
 browser = webdriver.Chrome()
-browser.get("https://en.wikipedia.org/wiki/Main_Page")
+browser.get("https://www.google.com")
 
-num = browser.find_element(By.XPATH, '//*[@id="articlecount"]/a[1]')
-print(num.text)
+search = browser.find_element(By.XPATH, '//*[@id="APjFqb"]')
+search.send_keys("Python")
+search.send_keys(Keys.ENTER)
+
+
+browser.quit()
