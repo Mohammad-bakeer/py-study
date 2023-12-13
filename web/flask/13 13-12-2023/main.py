3fc66faf4d562690add19eed1af60159aa27ee53
Mohammad Bakir <85129393+Mohammad-bakeer@users.noreply.github.com>
2023-12-13 10:46:18 +0300

    flask-wtf forms

---
+++ web/flask/13 13-12-2023/main.py
@@ -0,0 +1,29 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from flask import Flask, render_template
+from wtforms.validators import DataRequired
+
+
+class MyForm(FlaskForm):
+    name = StringField(label='name', validators=[DataRequired()])
+    password = PasswordField(label='password', validators=[DataRequired()])
+
+
+app = Flask(__name__)
+# csrf
+app.secret_key = "xcv"
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/login")
+def login():
+    form = MyForm()
+    return render_template("login.html", form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
