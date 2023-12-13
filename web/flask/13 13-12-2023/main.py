33758c9f6a9b05f5c2083848fe5764006afb7cab
Mohammad Bakir <85129393+Mohammad-bakeer@users.noreply.github.com>
2023-12-13 11:09:53 +0300

    added validation to the fields

--- web/flask/13 13-12-2023/main.py
+++ web/flask/13 13-12-2023/main.py
@@ -1,12 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from flask import Flask, render_template
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, Length
 
 
 class MyForm(FlaskForm):
-    name = StringField(label='Email', validators=[DataRequired()])
-    password = PasswordField(label='Password', validators=[DataRequired()])
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[
+                             DataRequired(), Length(min=8)])
     submit = SubmitField(label="Log In")
 
 
@@ -20,9 +21,10 @@ def home():
     return render_template('index.html')
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form = MyForm()
+    form.validate_on_submit()
     return render_template("login.html", form=form)
 
 
